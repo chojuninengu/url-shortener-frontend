c587ecd2328ea00412a95fd3f87a90ee390dd74e
chojuninengu <chojuninengu@gmail.com>
2025-03-15 09:02:32 +0100

    added functionalitied

--- src/index.js
+++ src/index.js
@@ -10,3 +10,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+import reportWebVitals from './reportWebVitals';
+
+reportWebVitals();
